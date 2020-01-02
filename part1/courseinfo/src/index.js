b89bec89f8dc9032c3137cc54a1137bb575776fa
dmasieh <dmasieh@gmail.com>
2020-01-01 23:01:01 -0500

    courseinfo Step 3: modified variable definitions so that instead of passing 6 redundant constants in the app component, we'll be passing in 3 object const variables with two key-value pairs each

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -2,16 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const Header = (props) => {
-	console.log(props)
-    return (
-        <div>
-            <h1>{props.course}</h1>
-        </div>
-    )
+    console.log(props)
+    //console logs only display in browser console, not terminal console
+    return <h1>{props.course}</h1>
 }
 
 const Part = (props) => {
     console.log(props)
+    //console logs only display in browser console, not terminal console
     return (
         <p>
             Part {props.exerciseNum}: {props.exerciseName}, {props.exerciseCount} exercises
@@ -20,7 +18,8 @@ const Part = (props) => {
 }
 
 const Content = (props) => {
-	console.log(props)
+    console.log(props)
+    //console logs only display in browser console, not terminal console
 	return (
 		<div>
             <Part exerciseNum='1' exerciseName={props.part1} exerciseCount={props.exercises1} />
@@ -33,6 +32,7 @@ const Content = (props) => {
 
 const Total = (props) => {
     console.log(props)
+    //console logs only display in browser console, not terminal console
     return(
         <div>
             <p>Number of exercises: {props.exercises1 + props.exercises2 + props.exercises3}</p>
@@ -43,29 +43,37 @@ const Total = (props) => {
 
 const App = () => {
     const course = 'Half Stack application development'
-    const part1 = 'Fundamentals of React'
-    const exercises1 = 10
-    const part2 = 'Using props to pass data'
-    const exercises2 = 7
-    const part3 = 'State of a component'
-    const exercises3 = 14
+    // instead of creating 6 more constant variables, let's just create 3 that are objects, that pass in 2 key-value pairs each
+    const part1 = {
+        name: 'Fundamentals of React',
+        exercises: 10
+    }
+    const part2 = {
+        name: 'Using props to pass data',
+        exercises: 7
+    }
+    const part3 = {
+        name: 'State of a component',
+        exercises: 14
+    }
 
+    // Just modifying what we pass in to the components so that it still works using the above
     return (
         <div>
         	<Header course={course}/>
             <Content 
-                part1={part1} 
-                part2={part2}
-                part3={part3}   
-                exercises1={exercises1}  
-                exercises2={exercises2}  
-                exercises3={exercises3}  
+                part1={part1.name} 
+                part2={part2.name}
+                part3={part3.name}   
+                exercises1={part1.exercises}  
+                exercises2={part2.exercises}  
+                exercises3={part3.exercises}  
             />
 
             <Total 
-                exercises1={exercises1}  
-                exercises2={exercises2}  
-                exercises3={exercises3}
+                exercises1={part1.exercises}  
+                exercises2={part2.exercises}  
+                exercises3={part3.exercises}
             />
 
         </div>
