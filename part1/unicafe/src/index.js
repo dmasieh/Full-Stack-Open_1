80d6e1d0e131dfaf81bf2957396efdc60e1211c4
dmasieh <dmasieh@gmail.com>
2020-01-04 14:29:55 -0500

    unicafe Steps 2, 3, and 4: expanded application so that more statistics about votes are displayed such as total votes counted, average rating of votes, and percentage of positive votes, extracted statistics and calculations into a separate component that I'll call VoteStats, and changed app to display statistics once feedback has been gathered.

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -9,6 +9,43 @@ const Button = (props) => (
 	</button>
 )
 
+const VoteStats = (props) => {
+	if(props.good >= 1 && (props.neutral + props.bad === 0)){
+        return (
+			<>
+				<h3>{props.stafTitle}</h3>	
+				<p>Good Votes: {props.good}</p>
+				<p>{props.totText}: {props.good}</p>
+				<p>There was only Good Votes, an Average of 1 and Positive Feedback!</p>
+				<p>.....so far</p>
+			</>	
+		)
+	} else if(props.good === 0 && props.neutral === 0 && props.bad === 0) {
+        return <h1>No Feedback Given Yet</h1>
+    } else {
+        return (
+			<>
+				<h3>{props.stafTitle}</h3>	
+				<p>Good votes: {props.good}</p>
+				<p>Neutral votes: {props.neutral}</p>
+				<p>Bad votes: {props.bad}</p>
+				<p>{props.totText}: {props.good + props.neutral + props.bad}</p>
+				<p>Average Score {
+					(
+						(props.good) + (props.bad * -1) 
+					) 
+						/
+					( 
+						(props.good) + (props.bad) + (props.neutral) 
+					)
+				}
+				</p>
+				<p>Percentage of Positive Feedback: {(props.good / (props.good + props.neutral + props.bad)) * 100}%</p>
+			</>
+		)
+	}
+}	   
+
 const App = () => {
   	// save clicks of each button to own state
   	const [good, setGood] = useState(0)
@@ -17,14 +54,17 @@ const App = () => {
 
   	return (
     	<div>
-			<h3>give feedback</h3>
+			<h3>Please Give Feedback</h3>
 			<Button text='Good' onClick={() => setGood(good + 1)} />
 			<Button text='Neutral' onClick={() => setNeutral(neutral+ 1)} />
 			<Button text='Bad' onClick={() => setBad(bad + 1)} />
-			<h3>statistics</h3>		    		
-			<p>good votes: {good}</p>
-			<p>neutral votes: {neutral}</p>
-			<p>bad votes: {bad}</p>
+			<VoteStats 
+				totText='Total # of Votes' 
+				stafTitle='Statistics of Feedback'
+				good={good} 
+				neutral={neutral} 
+				bad={bad} 
+			/>
 		</div>
   	)
 }
